b003c06c4f0f5a54d9273a479203bbd300c9ec3f
romi9035 <romilgvit@gmail.com>
2023-10-04 14:57:43 +0200

    second commit

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,21 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
-  "use strict";
-  new Text({
-    text: "Hello World",
-  }).placeAt("content");
-});
+// sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
+//   "use strict";
+//   XMLView.create({
+//     viewName: "sap.ui.demo.walkthrough.view.App",
+//   }).then(function (oView) {
+//     oView.placeAt("content");
+//   });
+// });
+sap.ui.define(
+  ["sap/ui/core/ComponentContainer"],
+  function (ComponentContainer) {
+    "use strict";
+    new ComponentContainer({
+      name: "sap.ui.demo.walkthrough",
+      settings: {
+        id: "walkthrough",
+      },
+      async: true,
+    }).placeAt("content");
+  }
+);
